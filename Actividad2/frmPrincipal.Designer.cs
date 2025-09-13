b89ae211c430f6857b57eb92b3243d55281409c5
fpablogaston <fpablogaston12@gmail.com>
2025-09-12 21:38:51 -0400

    Cree el formato del frmPrincipal con los formularios y las ventanas necesarias junto con su navegacion para completar la etapa 1

--- Actividad2/frmPrincipal.Designer.cs
+++ Actividad2/frmPrincipal.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmPrincipal";
+            menuStrip1 = new MenuStrip();
+            articuloToolStripMenuItem = new ToolStripMenuItem();
+            listarToolStripMenuItem = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            buscarToolStripMenuItem = new ToolStripMenuItem();
+            modificarToolStripMenuItem = new ToolStripMenuItem();
+            eliminarToolStripMenuItem = new ToolStripMenuItem();
+            verDetalleToolStripMenuItem = new ToolStripMenuItem();
+            marcasToolStripMenuItem = new ToolStripMenuItem();
+            administrarToolStripMenuItem = new ToolStripMenuItem();
+            categoriasToolStripMenuItem = new ToolStripMenuItem();
+            administrarToolStripMenuItem1 = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { articuloToolStripMenuItem, marcasToolStripMenuItem, categoriasToolStripMenuItem, salirToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // articuloToolStripMenuItem
+            // 
+            articuloToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { listarToolStripMenuItem, nuevoToolStripMenuItem, buscarToolStripMenuItem, modificarToolStripMenuItem, eliminarToolStripMenuItem, verDetalleToolStripMenuItem });
+            articuloToolStripMenuItem.Name = "articuloToolStripMenuItem";
+            articuloToolStripMenuItem.Size = new Size(81, 24);
+            articuloToolStripMenuItem.Text = "Articulos";
+          
+            // 
+            // listarToolStripMenuItem
+            // 
+            listarToolStripMenuItem.Name = "listarToolStripMenuItem";
+            listarToolStripMenuItem.Size = new Size(167, 26);
+            listarToolStripMenuItem.Text = "Listar";
+            listarToolStripMenuItem.Click += listarToolStripMenuItem_Click;
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(167, 26);
+            nuevoToolStripMenuItem.Text = "Buscar";
+            nuevoToolStripMenuItem.Click += nuevoToolStripMenuItem_Click;
+            // 
+            // buscarToolStripMenuItem
+            // 
+            buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            buscarToolStripMenuItem.Size = new Size(167, 26);
+            buscarToolStripMenuItem.Text = "Agregar ";
+            buscarToolStripMenuItem.Click += buscarToolStripMenuItem_Click;
+            // 
+            // modificarToolStripMenuItem
+            // 
+            modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            modificarToolStripMenuItem.Size = new Size(167, 26);
+            modificarToolStripMenuItem.Text = "Modificar";
+            modificarToolStripMenuItem.Click += modificarToolStripMenuItem_Click;
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            eliminarToolStripMenuItem.Size = new Size(167, 26);
+            eliminarToolStripMenuItem.Text = "Eliminar";
+            eliminarToolStripMenuItem.Click += eliminarToolStripMenuItem_Click;
+            // 
+            // verDetalleToolStripMenuItem
+            // 
+            verDetalleToolStripMenuItem.Name = "verDetalleToolStripMenuItem";
+            verDetalleToolStripMenuItem.Size = new Size(167, 26);
+            verDetalleToolStripMenuItem.Text = "Ver detalle ";
+            verDetalleToolStripMenuItem.Click += verDetalleToolStripMenuItem_Click;
+            // 
+            // marcasToolStripMenuItem
+            // 
+            marcasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { administrarToolStripMenuItem });
+            marcasToolStripMenuItem.Name = "marcasToolStripMenuItem";
+            marcasToolStripMenuItem.Size = new Size(70, 24);
+            marcasToolStripMenuItem.Text = "Marcas";
+            // 
+            // administrarToolStripMenuItem
+            // 
+            administrarToolStripMenuItem.Name = "administrarToolStripMenuItem";
+            administrarToolStripMenuItem.Size = new Size(169, 26);
+            administrarToolStripMenuItem.Text = "Administrar";
+            administrarToolStripMenuItem.Click += administrarToolStripMenuItem_Click;
+            // 
+            // categoriasToolStripMenuItem
+            // 
+            categoriasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { administrarToolStripMenuItem1 });
+            categoriasToolStripMenuItem.Name = "categoriasToolStripMenuItem";
+            categoriasToolStripMenuItem.Size = new Size(94, 24);
+            categoriasToolStripMenuItem.Text = "Categorias";
+            // 
+            // administrarToolStripMenuItem1
+            // 
+            administrarToolStripMenuItem1.Name = "administrarToolStripMenuItem1";
+            administrarToolStripMenuItem1.Size = new Size(224, 26);
+            administrarToolStripMenuItem1.Text = "Administrar";
+            administrarToolStripMenuItem1.Click += administrarToolStripMenuItem1_Click;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(52, 24);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "frmPrincipal";
+            Text = "frmPrincipal";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem articuloToolStripMenuItem;
+        private ToolStripMenuItem listarToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem buscarToolStripMenuItem;
+        private ToolStripMenuItem marcasToolStripMenuItem;
+        private ToolStripMenuItem administrarToolStripMenuItem;
+        private ToolStripMenuItem categoriasToolStripMenuItem;
+        private ToolStripMenuItem administrarToolStripMenuItem1;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem modificarToolStripMenuItem;
+        private ToolStripMenuItem eliminarToolStripMenuItem;
+        private ToolStripMenuItem verDetalleToolStripMenuItem;
     }
 }
--- Actividad2/frmPrincipal.cs
+++ Actividad2/frmPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Actividad2P3;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,59 @@ namespace Actividad2
         public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void listarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListarArticulos ventana = new frmListarArticulos();
+            ventana.Show();
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmBuscarArticulos ventana = new frmBuscarArticulos();
+            ventana.Show();
+        }
+
+        private void buscarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAgregarArticulos ventana = new frmAgregarArticulos();
+            ventana.Show();
+        }
+
+        private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmModificarArticulos ventana = new frmModificarArticulos();
+            ventana.Show();
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmEliminarArticulos ventana = new frmEliminarArticulos();
+            ventana.Show();
+        }
+
+        private void verDetalleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmVerDetalle ventana = new frmVerDetalle();
+            ventana.Show();
+        }
+
+        private void administrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmMarca ventana = new frmMarca();
+            ventana.Show();
+        }
+
+        private void administrarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmCategoria ventana = new frmCategoria();
+            ventana.Show();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
